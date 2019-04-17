ec5a66b86e1638571791cf9d6c516fa47ec518f2
Ishin Tokitsu <toki2222son@yahoo.co.jp>
2019-04-17 03:55:40 +0000

    adjust indent in users and blogs controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,59 +1,57 @@
 class BlogsController < ApplicationController
-    def new
-        @blog = Blog.new
+  def new
+    @blog = Blog.new
+  end
+    
+  def create
+    @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
+    if params[:back]
+      render "new"
+    elsif @blog.save
+      redirect_to blog_path(@blog.id),notice: "記事を投稿しました！！"
+    else
+      render "new"
     end
+  end
     
-    def create
-        @blog = Blog.new(blog_params)
-        @blog.user_id = current_user.id
-        if params[:back]
-            render "new"
-        elsif @blog.save
-            redirect_to blog_path(@blog.id),notice: "記事を投稿しました！！"
-        else
-            render "new"
-        end
+  def index
+    @blogs = Blog.all
+  end
+    
+  def show
+    @blog = Blog.find(params[:id])
+  end
+    
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path, notice: "投稿を削除しました！！"
+  end
+    
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+    
+  def update
+    @blog = Blog.find(params[:id])
+    if params[:back]
+      @blog.remove_image!
+      @blog.update(blog_params)
+      render "edit"
+    else
+      @blog.update(blog_params)
+      redirect_to blog_path(@blog.id),notice: "投稿を編集しました！！"
     end
+  end
     
-    def index
-        @blogs = Blog.all
-    end
+  def confirm
+    @blog = Blog.new(blog_params)
+  end
     
-    def show
-        @blog = Blog.find(params[:id])
-    end
+  private
     
-    def destroy
-        @blog = Blog.find(params[:id])
-        @blog.destroy
-        redirect_to blogs_path, notice: "投稿を削除しました！！"
-    end
-    
-    def edit
-        @blog = Blog.find(params[:id])
-    end
-    
-    def update
-        @blog = Blog.find(params[:id])
-        if params[:back]
-            @blog.remove_image!
-            @blog.update(blog_params)
-            render "edit"
-        else
-            @blog.update(blog_params)
-            redirect_to blog_path(@blog.id),notice: "投稿を編集しました！！"
-        end
-    end
-    
-    def confirm
-        @blog = Blog.new(blog_params)
-    end
-    
-    private
-    
-    def blog_params
-        params.require(:blog).permit(:title,:content,:image, :image_cache, :remove_image)
-    end
-    
-    
+  def blog_params
+    params.require(:blog).permit(:title,:content,:image, :image_cache, :remove_image)
+  end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ApplicationRecord
-    belongs_to:user
-    mount_uploader :image, ImageUploader
-    validates:title,presence: true
-    validates:content,presence: true
+  belongs_to:user
+  mount_uploader :image, ImageUploader
+  validates:title,presence: true
+  validates:content,presence: true
 end
